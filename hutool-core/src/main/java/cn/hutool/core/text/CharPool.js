828e320230472f8c01b07b938e6f0762d11350b1
jiangtao <jiangtao_cool@qq.com>
2021-12-28 22:28:28 +0800

    CharUtil.js

--- hutool-core/src/main/java/cn/hutool/core/text/CharPool.js
+++ hutool-core/src/main/java/cn/hutool/core/text/CharPool.js
@@ -1,86 +1,100 @@
-package cn.hutool.core.text;
 
 /**
  * 常用字符常量
- * @see StrPool
- * @author looly
- * @since 5.6.3
  */
-public interface CharPool {
+export default class CharPool {
 	/**
 	 * 字符常量：空格符 {@code ' '}
 	 */
-	char SPACE = ' ';
+	static SPACE = ' ';
+
 	/**
 	 * 字符常量：制表符 {@code '\t'}
 	 */
-	char TAB = '	';
+	static  TAB = '	';
+
 	/**
 	 * 字符常量：点 {@code '.'}
 	 */
-	char DOT = '.';
+	static  DOT = '.';
+
 	/**
 	 * 字符常量：斜杠 {@code '/'}
 	 */
-	char SLASH = '/';
+	static  SLASH = '/';
+
 	/**
 	 * 字符常量：反斜杠 {@code '\\'}
 	 */
-	char BACKSLASH = '\\';
+	static  BACKSLASH = '\\';
+
 	/**
 	 * 字符常量：回车符 {@code '\r'}
 	 */
-	char CR = '\r';
+	static  CR = '\r';
+
 	/**
 	 * 字符常量：换行符 {@code '\n'}
 	 */
-	char LF = '\n';
+	static  LF = '\n';
+
 	/**
 	 * 字符常量：减号（连接符） {@code '-'}
 	 */
-	char DASHED = '-';
+	static  DASHED = '-';
+
 	/**
 	 * 字符常量：下划线 {@code '_'}
 	 */
-	char UNDERLINE = '_';
+	static  UNDERLINE = '_';
+
 	/**
 	 * 字符常量：逗号 {@code ','}
 	 */
-	char COMMA = ',';
+	static  COMMA = ',';
+
 	/**
 	 * 字符常量：花括号（左） <code>'{'</code>
 	 */
-	char DELIM_START = '{';
+	static  DELIM_START = '{';
+
 	/**
 	 * 字符常量：花括号（右） <code>'}'</code>
 	 */
-	char DELIM_END = '}';
+	static  DELIM_END = '}';
+
 	/**
 	 * 字符常量：中括号（左） {@code '['}
 	 */
-	char BRACKET_START = '[';
+	static  BRACKET_START = '[';
+
 	/**
 	 * 字符常量：中括号（右） {@code ']'}
 	 */
-	char BRACKET_END = ']';
+	static  BRACKET_END = ']';
+
 	/**
 	 * 字符常量：双引号 {@code '"'}
 	 */
-	char DOUBLE_QUOTES = '"';
+	static  DOUBLE_QUOTES = '"';
+
 	/**
 	 * 字符常量：单引号 {@code '\''}
 	 */
-	char SINGLE_QUOTE = '\'';
+	static  SINGLE_QUOTE = '\'';
+
 	/**
 	 * 字符常量：与 {@code '&'}
 	 */
-	char AMP = '&';
+	static  AMP = '&';
+
 	/**
 	 * 字符常量：冒号 {@code ':'}
 	 */
-	char COLON = ':';
+	static  COLON = ':';
+
 	/**
 	 * 字符常量：艾特 {@code '@'}
 	 */
-	char AT = '@';
+	static  AT = '@';
 }
